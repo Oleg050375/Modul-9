763d919e90d3c4cafbf9abc2911bda0d3b3706a2
Oleg050375 <ov_varganov@mail.ru>
2024-10-07 08:55:25 +0500

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,11 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (abs(len(x[0]) - len(x[1])) for x in list(zip(first, second)) if not len(x[0]) == len(x[1]))
+
+second_result = (len(first[x]) == len(second[x]) for x in range(0, len(first)))
+
+# ТЕСТ ------------------------------------------------------------------------------------------------------
+
+print(list(first_result))
+print(list(second_result))
